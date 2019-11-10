139daa59e1a318a083e0ecd23435d6f1d6bdc9e5
shinhoseok <masin84@naver.com>
2019-11-10 14:01:16 +0900

    최초 커밋

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+import {Platform} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <WebView
+      source={{ uri: 'https://m.beauate.com' }}
+      style={{ marginTop: Platform.OS === 'ios' ? 20 : 0 }}
+    />
+  );
+};
+
+export default App;
